b5d0000423ec40936483ff66c49f5c001f07f393
Justin Dakay <justindakaycc@gmail.com>
2025-01-24 07:11:20 +0800

    first commit

---
+++ vite.config.js
@@ -0,0 +1,35 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            vue: 'vue/dist/vue.esm-bundler.js',
+        },
+    },
+    server: {
+        host: '127.0.0.1', // Force the use of IPv4
+        port: 5173, // Optional: Explicitly set the port
+        headers: {
+            'Access-Control-Allow-Origin': '*', // Allow cross-origin requests
+        },
+    },
+});
